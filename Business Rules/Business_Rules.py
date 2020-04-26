552209bb9a982739de8daa33c3c924a6373ea4de
Eric Dudgeon, CHFP, CRCR <62779300+EricDudgeon@users.noreply.github.com>
2020-04-26 00:09:02 -0500

    Add files via upload

--- Business Rules/Business_Rules.py
+++ Business Rules/Business_Rules.py
@@ -9,74 +9,80 @@ from tkinter import *
 from tkinter import ttk
 import pandas as pd
 import sqlite3
-data = pd.read_csv("Business Rules File.csv")
-dosage_forms = data["Dosage_Form"].astype("string").to_list()
+data = pd.read_csv("Business Rules File.csv", index_col=False)
+OPkeys = data["Keys"].astype("string").to_list()
 
-def openOP():
-    OP = Toplevel()
-    OP.title("Outpatient Business Rules")
-    OP.iconbitmap("icon.ico")
-    OP.geometry('{}x{}'.format(500, 600))
+# def openOP():
+#     OP = Toplevel()
+#     OP.title("Outpatient Business Rules")
+#     OP.iconbitmap("icon.ico")
+#     OP.geometry('{}x{}'.format(500, 600))
     
-    # create all of the OP containers
-    top_frame = Frame(OP, pady=10, padx=3)
-    center = Frame(OP, padx=3, pady=3)
-    btm_frame = Frame(OP, pady=3)
-    btm_frame2 = Frame(OP, pady=3)
+#     # create all of the OP containers
+#     top_frame = Frame(OP, pady=10, padx=3)
+#     center = Frame(OP, padx=3, pady=3)
+#     btm_frame = Frame(OP, pady=3)
+#     btm_frame2 = Frame(OP, pady=3)
     
-    # layout all of the OP containers
-    # OP.grid_rowconfigure(1, weight=1)
-    # OP.grid_columnconfigure(0, weight=1)
+#     # layout all of the OP containers
+#     # OP.grid_rowconfigure(1, weight=1)
+#     # OP.grid_columnconfigure(0, weight=1)
     
-    top_frame.grid(row=0,sticky=N+E+S+W)
-    center.grid(row=1, sticky=E+W)
-    btm_frame.grid(row=2)
-    btm_frame2.grid(row=3)
+#     top_frame.grid(row=0,sticky=N+E+S+W)
+#     center.grid(row=1, sticky=E+W)
+#     btm_frame.grid(row=2)
+#     btm_frame2.grid(row=3)
     
-    ## Adding OP Back Button
-    OPmenu = Menu(OP)
-    OP.config(menu=OPmenu)
-    OPsubMenu = Menu(menu)
-    OPmenu.add_cascade(label = "File", menu=OPsubMenu)
-    OPsubMenu.add_command(label="View All")
-    OPsubMenu.add_separator()
-    OPsubMenu.add_command(label="Back", command=OP.destroy)
+#     ## Adding OP Back Button
+#     OPmenu = Menu(OP)
+#     OP.config(menu=OPmenu)
+#     OPsubMenu = Menu(menu)
+#     OPmenu.add_cascade(label = "File", menu=OPsubMenu)
+#     OPsubMenu.add_command(label="View All")
+#     OPsubMenu.add_command(label="View Inactive")
+#     OPsubMenu.add_separator()
+#     OPsubMenu.add_command(label="Back", command=OP.destroy)
     
-
-    #Scroll Dropdown
-    OP_button = ttk.Combobox(top_frame, value=dosage_forms)
-    OP_button.grid(row=0, column=0,padx=10, pady=10,sticky=W)
-    OP_button.current(0)
+#     ##Creating Selected command FUCKING GENIUS Line of Code
+#     def Selected(event):
+#         DosageForm = data[data["Keys"] == OP_button.get()][["Dosage_Form"]]
+#         Form = DosageForm["Dosage_Form"]
+#         select = StringVar(Form)
+#         OP_form = Label.config(top_frame,text=select)
+#         OP_form.grid(row=1, column = 2, sticky=W,pady=10)
+        
+#     #Scroll Dropdown
+#     OP_button = ttk.Combobox(top_frame, value=keys)
+#     OP_button.grid(row=0, column=0,padx=10, pady=10,sticky=W)
+#     OP_button.current(0)
+#     OP_button.bind("<<ComboboxSelected>>", Selected)
+    
+            
+#     ## Adding labels to OP Left
+#     OP_form_label = Label(top_frame,text="Dosage Form:").grid(row=1, column = 0, sticky=W, pady=10)
+#     OP_form_abbrev_label = Label(top_frame,text="Dosage Form Abbrev").grid(row=2, column=0, sticky=W,pady=10)
+#     OP_sig_label = Label(top_frame,text="SIG Tool").grid(row=3, column=0, sticky=W,pady=10)
+    
+#     ## Aligned format
+#     space= Label(top_frame,text="").grid(row=0,column=1,padx=25)
+#     space1= Label(top_frame,text="").grid(row=1,column=1,padx=25)
+#     space3= Label(top_frame, text="").grid(row=2,column=1, padx=6)
+#     space3= Label(top_frame, text="").grid(row=3,column=1, padx=20)
+    
+#     ##Adding Labels to OP Right
+#     OPlabel = Label(top_frame,text="Outpatient Pharmacy Business Rules")
+#     OPlabel.grid(row=0, column=2, sticky=W)
+#     OP_form_abbrev = Label(top_frame,text="Tab").grid(row=2, column = 2, sticky=W,pady=10)
+#     OP_sig = Label(top_frame,text="TAB").grid(row=3, column=2, sticky=W,pady=10)
     
     
     
     
-    ## Adding labels to OP Left
-    OP_form_label = Label(top_frame,text="Dosage Form:").grid(row=1, column = 0, sticky=W, pady=10)
-    OP_form_abbrev_label = Label(top_frame,text="Dosage Form Abbrev").grid(row=2, column=0, sticky=W,pady=10)
-    OP_sig_label = Label(top_frame,text="SIG Tool").grid(row=3, column=0, sticky=W,pady=10)
     
-    ## Aligned format
-    space= Label(top_frame,text="").grid(row=0,column=1,padx=25)
-    space1= Label(top_frame,text="").grid(row=1,column=1,padx=25)
-    space3= Label(top_frame, text="").grid(row=2,column=1, padx=6)
-    space3= Label(top_frame, text="").grid(row=3,column=1, padx=20)
-    
-    ##Adding Labels to OP Right
-    OPlabel = Label(top_frame,text="Outpatient Pharmacy Business Rules")
-    OPlabel.grid(row=0, column=2, sticky=W)
-    OP_form = Label(top_frame,text="Tablet").grid(row=1, column = 2, sticky=W,pady=10)
-    OP_form_abbrev = Label(top_frame,text="Tab").grid(row=2, column = 2, sticky=W,pady=10)
-    OP_sig = Label(top_frame,text="TAB").grid(row=3, column=2, sticky=W,pady=10)
-    
-    
-    
-    
-    
-def openIP():
-    IP = Toplevel()
-    IP.title("Inpatient Business Rules")
-    IP.iconbitmap("icon.ico")
+# def openIP():
+#     IP = Toplevel()
+#     IP.title("Inpatient Business Rules")
+#     IP.iconbitmap("icon.ico")
     
 def openUpdate():
     update = Toplevel()
@@ -97,12 +103,12 @@ def information():
         
 root = Tk()
 root.title("Federal Business Rules")
-root.geometry('{}x{}'.format(250, 150))
+root.geometry('{}x{}'.format(325, 350))
 root.iconbitmap("icon.ico")
 
 
 # create all of the main containers
-top_frame = Frame(root, pady=3)
+top_frame = Frame(root, pady=0)
 center = Frame(root, padx=3, pady=3)
 btm_frame = Frame(root, pady=3)
 btm_frame2 = Frame(root, pady=3)
@@ -117,13 +123,13 @@ btm_frame.grid(row=3)
 btm_frame2.grid(row=4)
 
 label1 = Label(top_frame, text = "Federal Pharmacy Business Rules")
-label1.grid(row=0)
+label1.grid(row=0,column=0, columnspan=3, sticky=N, padx=25, pady=10)
 
-OPButton = Button(center, text="Outpatient", command=openOP)
-OPButton.grid(row=0, column=0, padx=10, pady=10)
+OPlabel = Label(top_frame, text="Outpatient")
+OPlabel.grid(row=1, column=0, padx=40, pady=0, sticky=W)
         
-IPButton = Button(center, text="Inpatient", command=openIP)
-IPButton.grid(row=0, column=1,padx=10, pady=10)
+IPlabel = Label(top_frame, text="Inpatient")
+IPlabel.grid(row=1, column=2,padx=50, pady=0, sticky=E)
 
 NTButton = Button(btm_frame2, text="Narrow Therp")
 NTButton.grid(row=0, column=0,padx=5, pady=5)
@@ -133,6 +139,62 @@ SIButton.grid(row=0, column=1,padx=5, pady=5)
 
 ExButton = Button(btm_frame2, text="Exceptions")
 ExButton.grid(row=0, column=2,padx=5, pady=5)
+
+#### Creating Outpatient
+
+def OPSelected(event):
+    OP = Toplevel()
+    OP.title("Outpatient Bussiness Rules")
+    OP.iconbitmap("icon.ico")
+    OP.geometry('{}x{}'.format(500, 600))
+    
+    # create all of the OP containers
+    top_frame = Frame(OP, pady=10, padx=3)
+    center = Frame(OP, padx=3, pady=3)
+    btm_frame = Frame(OP, pady=3)
+    btm_frame2 = Frame(OP, pady=3)
+    
+    # layout all of the OP containers
+    # OP.grid_rowconfigure(1, weight=1)
+    # OP.grid_columnconfigure(0, weight=1)
+    
+    top_frame.grid(row=0,sticky=N+E+S+W)
+    center.grid(row=1, sticky=E+W)
+    btm_frame.grid(row=2)
+    btm_frame2.grid(row=3)
+    back= Button(OP,text="Back",command=OP.destroy)
+    back.grid(row=0,column=0,sticky=E)
+    # Pulling in OP data
+    
+    DosageForm = data[data["Keys"] == OP_button.get()][["Dosage_Form"]]
+    form = Label(OP, text=DosageForm)
+    form.grid(row=0, column=1)
+    # OP_form = Label.config(top_frame,text=select)
+    # OP_form.grid(row=1, column = 2, sticky=W,pady=10)
+    
+#Scroll Dropdown
+OP_button = ttk.Combobox(top_frame, value=OPkeys)
+OP_button.grid(row=2, column=0,padx=2, pady=0,sticky=W)
+OP_button.current(0)
+OP_button.bind("<<ComboboxSelected>>", OPSelected)
+
+#### Creating Inpatient
+
+IPkeys = ["Eric", "Is", "A", "NERD"]
+
+def IPSelected(event):
+    print("YES")
+    # DosageForm = data[data["Keys"] == OP_button.get()][["Dosage_Form"]]
+    # Form = DosageForm["Dosage_Form"]
+    # select = StringVar(Form)
+    # OP_form = Label.config(top_frame,text=select)
+    # OP_form.grid(row=1, column = 2, sticky=W,pady=10)
+    
+#Scroll Dropdown
+IP_button = ttk.Combobox(top_frame, value=IPkeys)
+IP_button.grid(row=2, column=2,padx=2, pady=0,sticky=E)
+IP_button.current(0)
+IP_button.bind("<<ComboboxSelected>>", IPSelected)
 
 
 ## Creating the Menu bar
@@ -148,11 +210,18 @@ subMenu.add_command(label="Exit", command=root.destroy)
 
 updateMenu = Menu(menu)
 menu.add_cascade(label = "Update", menu=updateMenu)
-updateMenu.add_command(label="Database Path")
+updateMenu.add_command(label="Data File Path")
 updateMenu.add_separator()
 updateMenu.add_command(label="Add")
 updateMenu.add_command(label="Modify")
 updateMenu.add_command(label="Delete")
+
+viewMenu = Menu(menu)
+menu.add_cascade(label = "View", menu=viewMenu)
+viewMenu.add_command(label="All-OP")
+viewMenu.add_command(label="All-IP")
+viewMenu.add_separator()
+viewMenu.add_command(label="Inactive")
 
 
 root.mainloop()
